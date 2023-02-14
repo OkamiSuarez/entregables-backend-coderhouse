3ea000c5ae4124496713270115ea25883013fbaf
OkamiSuarez <okami97.backdev@hotmail.com>
2023-02-13 18:27:30 -0600

    Nuevos ajustes

--- main.js
+++ main.js
@@ -1,50 +1,17 @@
-// class ProductManager{
-//     constructor(id,title,description,price,thumbnail,code,stock){
-//         this.id = id;
-//         this.title = title;
-//         this.description = description;
-//         this.price = price;
-//         this.thumbnail = thumbnail;
-//         this.code = code;
-//         this.stock = stock;
-//     }
-//     getProducts(){
-//         // Creo que se tiene que usar map para poder generar el array que me van a pedir al principio 
-//         let products = [this.id, this.title, this.description, this.price, this.thumbnail, this.code, this.stock]
-//         console.log(products); 
-//     }
-//     addProduct(title,description,price,thumbnail,code,stock){
-//         if (id <= 1) {
-//             id++
-//         } else {
-//             id = 1
-//         }
-//         console.log(id);
-//         // new id = ProductManager(title,description,price,thumbnail,code,stock)
-//     }
-//     getProductById(id){
-//         // Probablemente se tiene que usar el metodo some para poder regresar con un booleano 
-//     }
-// }
+// Autoincremental
+let id = 1;
 
-// // ProductManager.getProducts();
-
-// // ProductManager.addProduct('producto prueba','Este es un producto prueba',200,'Sin imagen','abc123',25)
-
-// const productManager = new ProductManager();
-// // console.log(productManager);
-
-// // productManager.getProducts();
-
-// Intento alternativo
-
-let id = 1
-
+// Funcion que genera un autoincremental
 function autoincrement() {
     id++;
     return id;
-}
-const products = []
+};
+
+// Arrays
+const products = [];
+const sku = [];
+
+// Clase
 class ProductManager{
     constructor(title,description,price,thumbnail,code,stock){
         this.title = title;
@@ -55,31 +22,50 @@ class ProductManager{
         this.stock = stock;
     }
     getProducts(){
-        // Creo que se tiene que usar map para poder generar el array que me van a pedir al principio 
-        // let products = [this.title, this.description, this.price, this.thumbnail, this.code, this.stock]
         console.log(products); 
     }
     addProduct(title,description,price,thumbnail,code,stock){
-        // console.log(id);
-        // let products = [id,title,description,price,thumbnail,code,stock];
         products.push(id,title,description,price,thumbnail,code,stock)
         autoincrement();
-        // console.log(products); 
-        // console.log(id);
+        sku.push(code);
     }
     // getProductById(id){
     //     console.log(products[id]); 
     // }
 }
 
+// Se instancia
 const productManager = new ProductManager();
-// console.log(productManager);
 
+// Se llama a get products para array vacio
 productManager.getProducts();
+
+// Se agrega producto con su autoincremental ya declarado en el metodo
 productManager.addProduct('producto prueba','Este es un producto prueba',200,'Sin imagen','abc123',25);
-// console.log(id)
-// autoincrement();
-// console.log(id)
-productManager.getProducts();
-// productManager.getProductById(1);
 
+// Se llama a get products
+productManager.getProducts();
+
+
+
+// Pendiente
+productManager.addProduct('producto prueba','Este es un producto prueba',200,'Sin imagen','abc123',25);
+// productManager.getProducts();
+
+// for (const product of products){
+//     console.log(product.code)
+// }
+console.log(products[5]);
+if (products[5] === 'abc123'){
+    console.log('funciona')
+}else{
+    console.log('no funciona');
+}
+
+let evaluacion = products[5,5];
+console.log(evaluacion)
+
+let skuCode = sku;
+console.log(skuCode);
+
+console.log(stock);
