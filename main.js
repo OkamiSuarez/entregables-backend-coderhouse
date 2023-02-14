945a62d0c6a2643cd4654599ec50ae94d293c4fa
OkamiSuarez <okami97.backdev@hotmail.com>
2023-02-13 21:09:54 -0600

    Se ajusta la entrega con la nueva clase, aun falta ajustar cosas

--- main.js
+++ main.js
@@ -1,34 +1,41 @@
-// Autoincremental
-let id = 1;
-
-// Funcion que genera un autoincremental
-function autoincrement() {
-    id++;
-    return id;
-};
-
-// Arrays
-const products = [];
-const sku = [];
-
 // Clase
 class ProductManager{
-    constructor(title,description,price,thumbnail,code,stock){
-        this.title = title;
-        this.description = description;
-        this.price = price;
-        this.thumbnail = thumbnail;
-        this.code = code;
-        this.stock = stock;
+    constructor(){
+        this.products = [];
+        this.skus = []
     }
     getProducts(){
-        console.log(products); 
+        // console.log(products);
+        return console.log(this.products)
     }
+    
     addProduct(title,description,price,thumbnail,code,stock){
-        products.push(id,title,description,price,thumbnail,code,stock);
-        console.log(products.code);
-        autoincrement();
-        sku.push(code);
+
+        const id =
+            this.products.length === 0
+            ? 1
+            : this.products[this.products.length -1].id +1
+
+        const sku = {
+            id,
+            code
+        }
+        this.skus.push(sku)
+        // console.log('skuCode');
+        console.log(this.skus);
+        // console.log(this.skus.code)
+        // Me quede aqui, falta determinar como encontrar ese valor
+
+        const product = {
+            id,
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
+        }
+        this.products.push(product)
     }
     // getProductById(id){
     //     console.log(products[id]); 
@@ -52,24 +59,22 @@ productManager.getProducts();
 // Pendiente
 
 productManager.addProduct('producto prueba','Este es un producto prueba',200,'Sin imagen','abc123',25);
-// productManager.getProducts();
+productManager.getProducts();
 
-// for( const product of products){
-//     console.log(product.code)
+
+// console.log(products.code)
+// console.log(products[5]);
+// if (products[5] === 'abc123'){
+//     console.log('funciona')
+// }else{
+//     console.log('no funciona');
 // }
 
-console.log(products.code)
-console.log(products[5]);
-if (products[5] === 'abc123'){
-    console.log('funciona')
-}else{
-    console.log('no funciona');
-}
+// let evaluacion = products[5,5];
+// console.log(evaluacion)
 
-let evaluacion = products[5,5];
-console.log(evaluacion)
+// let skuCode = sku;
+// console.log(skuCode);
 
-let skuCode = sku;
-console.log(skuCode);
-
-console.log(stock);
+// console.log(stock);
+// console.log(products);
--- unused.js
+++ unused.js
@@ -35,3 +35,82 @@
 // // console.log(productManager);
 
 // // productManager.getProducts();
+
+
+// Segundo código 
+
+// Autoincremental
+let id = 1;
+
+// Funcion que genera un autoincremental
+function autoincrement() {
+    id++;
+    return id;
+};
+
+// Arrays
+const products = [];
+const sku = [];
+
+// Clase
+class ProductManager{
+    constructor(title,description,price,thumbnail,code,stock){
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.thumbnail = thumbnail;
+        this.code = code;
+        this.stock = stock;
+    }
+    getProducts(){
+        console.log(products); 
+    }
+    addProduct(title,description,price,thumbnail,code,stock){
+        products.push(id,title,description,price,thumbnail,code,stock);
+        console.log(products.code);
+        autoincrement();
+        sku.push(code);
+    }
+    // getProductById(id){
+    //     console.log(products[id]); 
+    // }
+}
+
+// Se instancia
+const productManager = new ProductManager();
+
+// Se llama a get products para array vacio
+productManager.getProducts();
+
+// Se agrega producto con su autoincremental ya declarado en el metodo
+productManager.addProduct('producto prueba','Este es un producto prueba',200,'Sin imagen','abc123',25);
+
+// Se llama a get products
+productManager.getProducts();
+
+
+
+// Pendiente
+
+productManager.addProduct('producto prueba','Este es un producto prueba',200,'Sin imagen','abc123',25);
+// productManager.getProducts();
+
+// for( const product of products){
+//     console.log(product.code)
+// }
+
+console.log(products.code)
+console.log(products[5]);
+if (products[5] === 'abc123'){
+    console.log('funciona')
+}else{
+    console.log('no funciona');
+}
+
+let evaluacion = products[5,5];
+console.log(evaluacion)
+
+let skuCode = sku;
+console.log(skuCode);
+
+console.log(stock);
