cba10721ee84ef219ee383d6426c5012b1ee104b
OkamiSuarez <okami97.backdev@hotmail.com>
2023-02-16 11:32:29 -0600

    Se hacen arreglos para la evaluacion

--- main.js
+++ main.js
@@ -11,18 +11,46 @@ class ProductManager{
     
     addProduct(title,description,price,thumbnail,code,stock){
 
+    // Auoincremental
         const id =
             this.products.length === 0
             ? 1
             : this.products[this.products.length -1].id +1
 
+    // Prototipo de evaluacion
+        this.skus[0] === undefined
+        ? console.log('array is empty')
+        : console.log('array has something');
+
+        // let skus_code = 
+        // // Puedo poner que si es un array vacio entonces que pase algo 
+        // this.skus[id - 1].code === undefined
+        // // ? skus_code = false
+        // ? console.log('This is true')
+        // // : console.log(this.skus.some((sku) => sku.code === skus_code));
+        // : console.log('This is false')
+
+        // console.log(skus_code);
+
+    // array de skus
         const sku = {
             id,
             code
         }
-        this.skus.push(sku)
-        console.log(this.skus);
 
+    // Push al array
+        this.skus.push(sku)
+    
+    // // Evaluacion de duplicidad
+    //     const skus_code = this.skus[id - 1].code
+    //     console.log(this.skus.some((sku) => sku.code === skus_code))
+    //     console.log(skus_code);
+
+    //     const duplicado = this.skus.find((sku) => sku.code === code)
+    //     console.log("duplicado")
+    //     console.log(duplicado + 'error')
+
+    // Array de product
         const product = {
             id,
             title,
@@ -34,10 +62,12 @@ class ProductManager{
         }
         this.products.push(product)
     }
+
     getProductById(id){
-        const skus_code = this.sku.code
-        console.log(skus_code);
-        // const eval = this.products.find((prod) => prod.code != )
+        const identifier = this.products[id - 1]
+        console.log(identifier);
+
+        // this.products.find((prod) => prod.code != )
         // return console.log(this.products.id === id)
         // return console.log(this.products[this.products.find((producto) => producto === id)])
     }
@@ -61,8 +91,11 @@ productManager.getProducts();
 
 // Pendiente
 
-// productManager.addProduct('producto prueba','Este es un producto prueba',200,'Sin imagen','abc123',25);
-// productManager.getProducts();
+productManager.addProduct('producto prueba 2','Este es un producto prueba 2',201,'Sin imagen','abc1234',251);
+productManager.getProducts();
 
-productManager.getProductById(1);
-productManager.getProductById(2);
+// productManager.getProductById(1);
+// productManager.getProductById(2);
+// productManager.getProductById(2);
+// productManager.getProductById(1);
+// productManager.getProductById(3);
