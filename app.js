7fc25a4bc18b74be077b8d54d85a8a8b8bc7a45a
OkamiSuarez <okami97.backdev@hotmail.com>
2023-03-07 16:17:51 -0600

    se guarda con comentarios para poder tener el backup

--- app.js
+++ app.js
@@ -43,21 +43,43 @@ app.get('/products',async (req,res)=>{
     const products = await productManager.getProducts()
     // console.log(req.query);
     const {limit} = req.query
-    console.log('limit');
-    console.log(limit);
-    console.log('limit');
+    const forLength = products.length
+    // console.log("forLength");
+    // console.log(forLength);
+    // console.log('limit');
+    // console.log(limit);
+    // console.log('limit');
     // console.log(limitValue);
-    console.log(products);
-    if(limit){
-        console.log('Estableciendo limite');
+    // console.log(products);
+    if(limit>forLength){
+        // Esta evaluación es para evitar que se soliciten
+        // MAS productos de los que existen
+        res.json({products})
+    }else if (limit<=forLength){
+        // console.log('Estableciendo limite');
+        // let requestedProduct = await productManager.getProductById(+limit)
+        let queryArray = []
+        // console.log(requestedProduct);
+        let evaluateLimit = parseInt(limit) + 1
+        // console.log(evaluateLimit);
         // console.log(limitValue -1);
-        // for(let i = 0; i < limitValue; i++){
-        //     // console.log(products[i])
-        //     let limitedProducts = products[i]
-        //     console.log(limitedProducts);
-        //     res.json({limitedProducts})
-        // }
-        req.json({limit})
+        for(let i = 1; i < evaluateLimit; i++){
+            // console.log(i)
+            let limitArray = await productManager.getProductById(i)
+
+            // let limitArray =+ [{requestedProduct}]
+            console.log(limitArray);
+            queryArray.push(limitArray)
+            console.log(queryArray);
+
+            // console.log(requestedProduct);
+            // res.json({requestedProduct})
+            // console.log('result del limit');
+            // console.log(limitResult);
+            // return limitResult
+            // return queryArray
+        }
+        res.json({queryArray})
     }else{
         res.json({products})
     }
